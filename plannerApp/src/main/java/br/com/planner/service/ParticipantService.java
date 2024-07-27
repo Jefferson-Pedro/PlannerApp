e3f07a46b4013f74333cf5fa4fa0ac563195f632
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-07-27 14:11:19 -0300

    Update ParticipantService.java
    - Criação do método com a lógica de registro de evento;
    - Criação do método com a lógica de confirmação do evento na classe Service;

--- plannerApp/src/main/java/br/com/planner/service/ParticipantService.java
+++ plannerApp/src/main/java/br/com/planner/service/ParticipantService.java
@@ -3,12 +3,51 @@ package br.com.planner.service;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import br.com.planner.dto.ParticipantCreateResponse;
+import br.com.planner.dto.ParticipantData;
+import br.com.planner.model.Participant;
+import br.com.planner.model.Trip;
+import br.com.planner.repository.ParticipantRepository;
 
 @Service
 public class ParticipantService {
 	
-	public void registerParticipantsToEvent(List<String> participantsToInvate, UUID id) {}
+	@Autowired
+	private ParticipantRepository repository;
+	
+	public void registerParticipantsToEvent(List<String> participantsToInvate, Trip trip) {
+		
+		List<Participant> participants = participantsToInvate.stream().map(email -> new Participant(email, trip)).toList();
+		
+		this.repository.saveAll(participants);
+		
+		System.out.println("Id Participante: " + participants.get(0).getId());
+	}
+	
+	public ParticipantCreateResponse registerParticipantsToEvent(String email, Trip trip) {
+		Participant newParticipant = new Participant(email, trip);
+		this.repository.save(newParticipant);
+		
+		return new ParticipantCreateResponse(newParticipant.getId());
+	}
 	
 	public void triggerConfirmationEmailToParticipants(UUID tripId) {}
+	
+	public void triggerConfirmationEmailToParticipants(String email) {}
+
+	public List<ParticipantData> getAllParticipantsFromEvent(UUID id) {
+	
+		return this.repository
+				.findByTripId(id)
+				.stream()
+				.map(participant -> 
+				new ParticipantData(
+						participant.getId(), 
+						participant.getName(), 
+						participant.getEmail(),
+						participant.getIsConfirmed())).toList();
+	}
 }
